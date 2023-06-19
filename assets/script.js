5c745471a9dde23a9e5413cc4767016173aacecf
charla winbush <charla.winbush@outlook.com>
2023-06-18 20:40:18 -0700

    push

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,84 @@
-const products = [cherry, strawberry, orange];
 
-
-const cherry = {
-  name: "cherry",
-  price: 6,
-  quantity: 0,
-  productId: 1,
-  Image: "../images/cherry.jpeg",
-};
-const strawberry = {
-  name: "strawberry",
+  const products= [
+    {
+    name:"cherry",
+    price: 6,
+    quantity: 0,
+    productId: 1,
+    image: "images/cherry.jpg",
+  }
+{
+  name: "orange",
   price: 6,
   quantity: 0,
   productId: 2,
-  Image: "../images/strawberry.jpeg",
+  image: "images/orange.jpg",
+}
+{
+{name: "strawberry",
+price: 6,
+quantity: 0,
+productId: 3,
+image: "images/strawberry",
+}
+];
 
-  const orange = {
-    name: "orange",
-    price: 6,
-    quantity: 0,
-    productId: 3,
-  
+let cart =[];
+
+
+function addProductToCart(productId)
+products.forEach(product => {
+  if (product.productId ==  products
+    product.quantity += 1;
+    if (cart.includes (product))
+    cart.push(product);
+}
+
+function increaseQuantity(productId) 
+  products.forEach(product => {
+    if (product.productId == product)
+    product.quantity +=1;
+  }
 
 
 
-)
-
-/* 
-
-/* Images provided in /images folder. All images from Unsplash.com
-   - cherry.jpg by Mae Mu
-   - orange.jpg by Mae Mu
-   - strawberry.jpg by Allec Gomes
-*/
-
-/* Declare an empty array named cart to hold the items in the cart */
-
-/* Create a function named addProductToCart that takes in the product productId as an argument
-  - addProductToCart should get the correct product based on the productId
-  - addProductToCart should then increase the product's quantity
-  - if the product is not already in the cart, add it to the cart
-*/
 
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
 
+
+function decreaseQuantity(productId) {
+products.forEach(product) =>{
+  if (product.productId === productId){
+    product.quantity-= 1;
+    if ((product.quantity === 0)){
+      if (index  !==-1) {
+        cart.splice(lndex, 1);
+      }
+    
+  
+}
+
+
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
+function removeProductFromCart(productId) {
+  for (let i =0; i  = < cart.length;
+    i++) {
+     if (cart[[]i].productId === productId) {
+      cart.splice(i,1);
+      console.console.log( ('Remove product with productId'));
+      break;
+     }
 
+
+     
+}
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
@@ -63,9 +89,10 @@ const strawberry = {
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
-
+function emptyCart
 /* Create a function called emptyCart that empties the products from the cart */
 
+}
 /* Create a function named pay that takes in an amount as an argument
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
